7d1a43064b5f923c2f2ad0fb59e1e16f9bd2a2f8
Gia Mule <gqm2162@rit.edu>
2020-09-30 11:45:37 -0700

    make analyze.sh more robust; create gitignore

--- analyze.sh
+++ analyze.sh
@@ -6,7 +6,20 @@ if [[ $path_in = */ ]]
 then
 	path=$path_in
 else
-	path=$path_in/
+	path="${path_in}/"
+fi
+
+echo Create backup for files? Yes or No
+echo Note: You will not be able to run cleanup without this directory
+read backup
+
+if [[ $backup -eq 'Yes' ]]
+then
+	backup_path=$(echo ${path_in%/} | awk 'BEGIN{FS=OFS="/"}{NF--; print}')/backup_files
+	mkdir $backup_path
+	cp ${path}* $backup_path
+	echo Backup located at $backup_path
+	ls $backup_path
 fi
 
 echo Extracting files from $path
@@ -20,3 +33,16 @@ echo Analysis complete. See results/ for full results.
 
 head -n 1 results/taglist.txt
 head -n 1 results/unchanged_tags.txt
+
+echo Run cleanup? Yes or Not
+echo Note: This will revert all changed files back to their originals.
+read cleanup
+
+if [[ $cleanup -eq 'Yes' ]]
+then
+	rm -rf $path
+	mkdir $path
+	cp $backup_path/* $path
+	echo Original files restored to $path
+	ls $path
+fi
--- cleanup.sh
+++
@@ -1,3 +0,0 @@
-rm -rf ../../files
-mkdir ../../files
-cp ../../backup_files/* ../../files
